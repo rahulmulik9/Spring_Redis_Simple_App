30263f43e211c44f5d8c6d89fe1f3e9bc6c57ed9
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-21 21:33:11 +0530

    Added redis config this will return RedisCacheManager which work for product class

---
+++ Phase-3-spring-cache-annotation/minishop/src/main/java/com/rahul/minishop/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.rahul.minishop.config;
+
+import com.rahul.minishop.entity.Product;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        // Jackson 3 typed serializer: Product <-> JSON
+        JacksonJsonRedisSerializer<Product> valueSerializer = new JacksonJsonRedisSerializer<>(Product.class);
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(60))   // default TTL for every cache
+                .disableCachingNullValues()         // caching nulls comes later
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+}
